b1fe0af54ddaef61f33a77073db47906a2c1b3fe
Eagle <84711967+EAGLE1309@users.noreply.github.com>
2022-01-08 18:13:38 +0530

    Update add.cpp

--- maths/add.cpp
+++ maths/add.cpp
@@ -1,1 +1,18 @@
-#include<iostream.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+
+    cout << "Enter first number: ";
+    cin >> a;
+
+    cout << "Enter second number: ";
+    cin >> b;
+
+    c = a + b;
+    cout << "The sum is: " << c;
+
+    return 0;
+}
